c455bcbda29b6115baabf1803c331f29ddde318e
Daniel A <dan.ataiyero@hotmail.com>
2024-02-22 23:51:57 +0000

    Improved statements and winning constant

--- SoltMachine/Program.cs
+++ SoltMachine/Program.cs
@@ -11,10 +11,11 @@ namespace SoltMachine
 
             const int NUMBER_OF_ROWS = 3;
             const int NUMBER_OF_COLUMNS = 3;
+            const int WINNINGS = 1;
             
             Random random = new Random();
 
-            int availableBalance = 10;
+            int availableBalance = 10000;
 
             bool gameIsOn = true;
             bool matchFound = false;
@@ -27,87 +28,96 @@ namespace SoltMachine
                 Console.Write("How much would you like to bet? ");
                 int bet = Convert.ToInt32(Console.ReadLine());
                 
-                Console.WriteLine($"£{bet} locked in.");
+                if (bet > availableBalance)
+                {
+                    Console.WriteLine("Not enough cash ");
+                   
+                }
+                else
+                {
 
-                availableBalance -= bet;
-            
-                int[,] numbers = new int[NUMBER_OF_ROWS, NUMBER_OF_COLUMNS];
-                
-                
-                for (int i = 0; i < numbers.GetLength(0); i++)
-                {
-                    for (int j = 0; j < numbers.GetLength(1); j++)
+                    Console.WriteLine($"£{bet} bet locked in.");
+
+                    availableBalance -= bet;
+
+                    int[,] numbers = new int[NUMBER_OF_ROWS, NUMBER_OF_COLUMNS];
+
+
+                    for (int i = 0; i < numbers.GetLength(0); i++)
                     {
-                        numbers[i, j] = random.Next(1, 10); 
+                        for (int j = 0; j < numbers.GetLength(1); j++)
+                        {
+                            numbers[i, j] = random.Next(1, 10);
+                        }
                     }
-                }
-                
-                Console.WriteLine("Slot Numbers:");
-                for (int i = 0; i < numbers.GetLength(0); i++)
-                {
-                    for (int j = 0; j < numbers.GetLength(1); j++)
+
+                    Console.WriteLine("Slot Numbers:");
+                    
+                    for (int i = 0; i < numbers.GetLength(0); i++)
                     {
-                        Console.Write(numbers[i, j] + " ");
+                        for (int j = 0; j < numbers.GetLength(1); j++)
+                        {
+                            Console.Write(numbers[i, j] + " ");
+                        }
+
+                        Console.WriteLine();
                     }
-                    Console.WriteLine();
-                }
-                
-                if (matchFound)
-                {
-                    availableBalance += 1;
-                }
-                
-                if (availableBalance == 0)
-                {
-                    Console.WriteLine("No funds left you lose.");
-                    gameIsOn = false;
-                }
-                
-                
-                for (int i = 0; i < numbers.GetLength(0); i++)
-                {
-                    if (numbers[i, 0] == numbers[i, 1] && numbers[i, 1] == numbers[i, 2])
+
+                    if (availableBalance == 0)
+                    {
+                        Console.WriteLine("No funds left you lose.");
+                        gameIsOn = false;
+                    }
+
+
+                    for (int i = 0; i < NUMBER_OF_ROWS; i++)
+                    {
+                       
+                        if (numbers[i, 0] == numbers[i, 1] && numbers[i, 1] == numbers[i, 2])
+                        {
+                            availableBalance += WINNINGS;
+                            Console.WriteLine($"Matching numbers found you won £1.");
+                            matchFound = true;
+                        }
+
+                    }
+
+                    // vertical
+                    for (int j = 0; j < NUMBER_OF_COLUMNS; j++)
+                    {
+                        if (numbers[0, j] == numbers[1, j] && numbers[1, j] == numbers[2, j])
+                        {
+                            availableBalance += WINNINGS;
+                            Console.WriteLine($"Matching numbers found you won £1.");
+                            matchFound = true;
+                        }
+
+                    }
+
+                    // diagonal
+                    if (numbers[0, 0] == numbers[1, 1] && numbers[1, 1] == numbers[2, 2])
                     {
                         Console.WriteLine($"Matching numbers found you won £1.");
+                        availableBalance += WINNINGS;
                         matchFound = true;
                     }
-                  
-                }
 
-                // vertical
-                for (int j = 0; j < numbers.GetLength(1); j++)
-                {
-                    if (numbers[0, j] == numbers[1, j] && numbers[1, j] == numbers[2, j])
+                    if (numbers[0, 2] == numbers[1, 1] && numbers[1, 1] == numbers[2, 0])
                     {
                         Console.WriteLine($"Matching numbers found you won £1.");
+                        availableBalance += WINNINGS;
                         matchFound = true;
                     }
-                  
-                }
+                    
+                    if (matchFound)
+                    {
+                        availableBalance +=  bet;
+                    }
+                    
+                    Console.WriteLine($"Your remaining balance is £{availableBalance}");
 
-                // diagonal
-                if (numbers[0, 0] == numbers[1, 1] && numbers[1, 1] == numbers[2, 2])
-                {
-                    Console.WriteLine($"Matching numbers found you won £1.");
-                    matchFound = true;
                 }
-               
-                if (numbers[0, 2] == numbers[1, 1] && numbers[1, 1] == numbers[2, 0])
-                {
-                    Console.WriteLine($"Matching numbers found you won £1.");
-                    matchFound = true;
-                }
-              
-                
-               
-
-                
-                Console.WriteLine($"Your remaining balance is £{availableBalance}");
-                
             }
-
-          
-            
         }
     }
 }
